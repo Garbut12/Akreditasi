319a0f8fb58a75ca234a9d1c1d8cc3c75e3bf483
Garbut12 <muhammadardhiyansyah16@gmail.com>
2019-11-13 21:04:20 +0700

    tahap kedua login

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,9 +3,16 @@
 
     class Auth extends CI_Controller
     {
+        public function __construct()
+        {
+            parent::__construct();
+            $this->load->library('form_validation');
+        }
+
         public function index()
         {
-            $this->load->view('templates/auth_header');
+            $data['title'] = 'Akreditasi Login BAN PAUD';
+            $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/login');
             $this->load->view('templates/auth_footer');
         }
@@ -13,8 +20,42 @@
 
         public function registration()
         {
-            $this->load->view('templates/auth_header');
-            $this->load->view('auth/registration');
-            $this->load->view('templates/auth_footer');
+
+            $this->form_validation->set_rules('name', 'Name', 'required|trim');
+            $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]',[
+                'is_unique' => 'this email has already registered'
+            ]);
+            $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]',[
+                'matches' => 'password dont match!',
+                'min_length' => 'password to short'
+            ]);
+            $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+
+
+            if ($this->form_validation->run() == false) {
+                $data['title'] = 'Akreditasi Registration BAN PAUD';
+                $this->load->view('templates/auth_header', $data);
+                $this->load->view('auth/registration');
+                $this->load->view('templates/auth_footer');
+            }else{
+                $data=[
+                    'name'=>$this->input->post('name',true),
+                    email=>$this->input->post('email',true),
+                    'image' => 'default.jpg',
+                    'password' => password_hash($this->input->post('password'), PASSWORD_DEFAULT),
+                    'role_id' => 2,
+                    'is_active' => 1,
+                    'date_created' => time()
+                ];
+
+                $this->db->insert('user', $data);
+                $this->session->set_flashdata('message',
+                    '<div class="alert alert-success" role="alert">
+                          Congrulatulation your account  hass been crated, please Login !!
+                     </div>');
+                redirect('auth');
+            }
+
         }
     }
